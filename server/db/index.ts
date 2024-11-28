118d7aeb667eac4812a4501ab1e675e314d433de
Alysha Dosdos <alyshadosdos@gmail.com>
2024-11-29 07:32:24 +1300

    show all locations and events for a day

--- server/db/index.ts
+++ server/db/index.ts
@@ -9,7 +9,25 @@ const environment = (process.env.NODE_ENV || 'development') as Environment
 const config = knexFile[environment]
 export const connection = knex(config)
 
-export async function getAllLocations() {
-  const locations: unknown[] = [] // TODO: replace this with your knex query
+export async function getAllLocations(db = connection) {
+  const locations = await db('locations').select('*')
   return locations as Location[]
+}
+
+export async function getEventsByDay(
+  day: string,
+): Promise<EventWithLocation[]> {
+  const db = connection
+  const events = await db('events')
+    .join('locations', 'events.location_id', 'locations.id')
+    .select(
+      'events.id as id',
+      'events.day',
+      'events.time',
+      'events.name as eventName',
+      'events.description as description',
+      'locations.name as locationName',
+    )
+    .where('events.day', day)
+  return events as EventWithLocation[]
 }
--- server/routes/locations.ts
+++ server/routes/locations.ts
@@ -8,22 +8,9 @@ const router = express.Router()
 router.get('/', async (req, res, next) => {
   try {
     // TODO: Replace this with all of the locations in the database
-    const locations = [
-      {
-        id: 1,
-        name: 'TangleStage',
-        description:
-          'Not the biggest stage, but perhaps the most hip. Not the biggest stage, but perhaps the most hip. Not the biggest stage, but perhaps the most hip.',
-      },
-      {
-        id: 2,
-        name: 'Yella Yurt',
-        description:
-          "It's a freakin' yurt! Get in here! It's a freakin' yurt! Get in here! It's a freakin' yurt! Get in here! It's a freakin' yurt! Get in here!",
-      },
-    ]
+    await db.getAllLocations()
 
-    res.json({ locations })
+    res.json({ location })
   } catch (e) {
     next(e)
   }
